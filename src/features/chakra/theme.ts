88462b4127c870e534e7622f376f0d12f984de68
Samuel Maréchal <20586745+marechalsamuel@users.noreply.github.com>
2024-06-17 00:56:38 +0200

    no top awiait

--- src/features/chakra/theme.ts
+++ src/features/chakra/theme.ts
@@ -1,12 +1,9 @@
 import { extendTheme } from '@chakra-ui/react'
 import { linkTheme } from "./Link.theme";
-import { storage } from '../storage/storage';
-
-const { colorMode } = await storage?.local.get("colorMode") || {};
 
 const theme = extendTheme({
   config: {
-    initialColorMode: colorMode || 'system',
+    initialColorMode: 'system',
     useSystemColorMode: false,
   },
   components: {
