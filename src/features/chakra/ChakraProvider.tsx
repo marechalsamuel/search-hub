cbfe303e4377d179fb030178adb9a83cdc5811f6
Samuel Maréchal <20586745+marechalsamuel@users.noreply.github.com>
2024-06-17 00:12:38 +0200

    Synced ColorMode

--- src/features/chakra/ChakraProvider.tsx
+++ src/features/chakra/ChakraProvider.tsx
@@ -1,20 +1,33 @@
+import { useEffect } from "react";
+import { LdsColorMode } from "./ldsColorMode/ldsColorMode.hook.ts";
 import theme from "./theme.ts";
 import {
   ColorModeScript,
   ChakraProvider as OriginalChakraProvider,
-  ChakraProviderProps as OriginalChakraProviderProps,
+  ChakraProviderProps,
+  useColorMode,
 } from "@chakra-ui/react";
+import useExtensionStorage from "../storage/extenstionStorage.hook.ts";
 
-export type ChakraProviderProps = OriginalChakraProviderProps;
+const ColorMode = () => {
+  const { storedData: storedColorMode } =
+    useExtensionStorage<LdsColorMode>("colorMode", "system");
+
+  const { colorMode, setColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (storedColorMode && storedColorMode !== colorMode) {
+      setColorMode(storedColorMode);
+    }
+  }, [storedColorMode, colorMode, setColorMode]);
+
+  return <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+};
 
 export const ChakraProvider = ({ children, ...props }: ChakraProviderProps) => {
   return (
-    <OriginalChakraProvider
-      {...props}
-      cssVarsRoot="#crx-root"
-      theme={theme}
-    >
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <OriginalChakraProvider {...props} cssVarsRoot="#crx-root" theme={theme}>
+      <ColorMode />
       {children}
     </OriginalChakraProvider>
   );
